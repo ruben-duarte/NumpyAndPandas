c6f1a2afa17508cd0f0af3c174a3d31080f5446d
Ruben Dario Duarte Anaya <ruben.duarte.anaya@gmail.com>
2023-05-02 15:32:37 -0500

    exploring numpy

--- 01_numpy.py
+++ 01_numpy.py
@@ -29,3 +29,47 @@ b.itemsize
 a.size  + a.itemsize
 a.nbytes
 
+#accessing / changing specific elements .... rows, columns
+
+a = np.array([[1,2,3,4,5,6,7],[8,9,10,11,12,13,14]])
+print(a)
+
+#get specific elements [row, column] count from 0
+
+#get 12
+a[1,4]
+a[1,-3]  #is the same
+
+#get an specific row
+a[0, :]
+#get an specific column
+a[: , 2]
+#more fancy [startindex:endindex:stepsize]
+a[0, 1:6:2 ]
+a[0, 1:-1:2 ]
+
+#changevalue
+a[1,5] = 20
+
+a[: , 2] = 5
+a[: , 2] = [1,2]
+
+#3d example
+
+b = np.array([[[],[]],[[],[]]])
+b = np.array([[[1,2],[3,4]],[[5,6],[7,8]]])
+print(b)
+b[0,1,1]
+b[:,1,:]
+#replace
+b[ : , 1, :] = [[9,9],[8,8]]
+
+
+
+
+
+
+
+
+
+
